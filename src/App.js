e39f606b6e96f48cc524085f1207aa49b4ab655c
AzaJS <aziretbp001@gmail.com>
2021-12-13 18:10:35 +0600

    added create and context

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import AddProduct from './components/AddProduct/AddProduct';
+import "antd/dist/antd.css"
+import "./App.css"
+import ProductsList from './components/ProductsList/ProductsList';
 
 const App = () => {
   return (
     <div>
-      Hi Janar
+      <AddProduct />
+      <ProductsList />
     </div>
   );
 };
--- src/contexts/productsContext.js
+++ src/contexts/productsContext.js
@@ -1,11 +1,44 @@
-import React from 'react';
+import {
+  collection,
+  deleteDoc,
+  doc,
+  onSnapshot,
+  query,
+} from "firebase/firestore";
+import React, { useEffect, useState } from "react";
+import { db } from "../firebase";
 
-const productsContext = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+export const productsContext = React.createContext();
+
+const ProductsContextProvider = ({ children }) => {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    const q = query(collection(db, "products"));
+    const unsub = onSnapshot(q, (querySnapshot) => {
+      let productsArray = [];
+      querySnapshot.forEach((doc) => {
+        // itemsArray.push(doc.data())
+        productsArray.push({ ...doc.data(), id: doc.id });
+      });
+      setProducts(productsArray);
+    });
+  }, []);
+  console.log(products);
+
+  const handleDelete = async (id) => {
+    await deleteDoc(doc(db, "products", id));
+  };
+
+  return (
+    <productsContext.Provider
+      value={{
+        products,
+        handleDelete,
+      }}
+    >
+      {children}
+    </productsContext.Provider>
+  );
 };
 
-export default productsContext;
+export default ProductsContextProvider;
