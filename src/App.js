d2608c71685e8847d3d7ac7b2ec37d20aa3f96a2
tilekov <tilekovjanar1@gmail.com>
2021-12-14 18:02:55 +0600

    auth/registr, footer, edit home

--- src/App.js
+++ src/App.js
@@ -3,23 +3,26 @@ import React from "react";
 import "./App.css";
 import "antd/dist/antd.css";
 import { BrowserRouter } from "react-router-dom";
+import ProductsContextProvider from "./contexts/productsContext";
+import AuthContextProvider from "./contexts/authContext";
 
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import AddProduct from "./components/AddProduct/AddProduct";
-import ProductsContextProvider from "./contexts/productsContext";
 import ProductsList from "./components/ProductsList/ProductsList";
 import Routing from "./Routing";
 
 const App = () => {
   return (
-    <ProductsContextProvider>
-    <BrowserRouter>
-      <Header />
-      <Routing />
-      <Footer />
-    </BrowserRouter>
-    </ProductsContextProvider>
+    <AuthContextProvider>
+      <ProductsContextProvider>
+        <BrowserRouter>
+          <Header />
+          <Routing />
+          <Footer />
+        </BrowserRouter>
+      </ProductsContextProvider>
+    </AuthContextProvider>
   );
 };
 
