8fc1d2325dec9e33a562498145cd07404f50f825
tilekov <tilekovjanar1@gmail.com>
2021-12-16 11:44:07 +0600

    added filters

--- src/components/ProductsList/MensProductsList.jsx
+++ src/components/ProductsList/MensProductsList.jsx
@@ -2,6 +2,7 @@ import { Empty, Input, Pagination } from "antd";
 import React, { useContext, useEffect, useState } from "react";
 import { useSearchParams } from "react-router-dom";
 import { productsContext } from "../../contexts/productsContext";
+import Filters from "../Filters/Filters";
 
 import ProductsCard from "../ProductsCard/ProductsCard";
 
@@ -16,13 +17,19 @@ const MensProductsList = () => {
   const [limit, setLimit] = useState(
     searchParams.get("_limit") ? searchParams.get("_limit") : 4
   );
-  const { getProducts, products, productsTotalCount } = useContext(productsContext);
+  const { getProducts, products, productsTotalCount } =
+    useContext(productsContext);
+  const [type, setType] = useState([]);
+  const [price, setPrice] = useState([1, 1000000]);
 
   useEffect(() => {
     setSearchParams({
       q: search,
       _page: page,
+      type: type,
       _limit: limit,
+      price_gte: price[0],
+      price_lte: price[1],
     });
   }, []);
   useEffect(() => {
@@ -32,9 +39,12 @@ const MensProductsList = () => {
     setSearchParams({
       q: search,
       _page: page,
+      type: type,
       _limit: limit,
+      price_gte: price[0],
+      price_lte: price[1],
     });
-  }, [search, page, limit]);
+  }, [search, page, type, limit, price]);
   //   console.log(products)
   let list = products.filter((item) => {
     return item.gender == "Men";
@@ -42,12 +52,24 @@ const MensProductsList = () => {
   return (
     <div className="container">
       <div className="serach-prod">
-      <Input.Search
-          value={search}
-          onChange={(e) => setSearch(e.target.value)}
-          style={{ width: "25vw" }}
-          placeholder="Search..."
-        />
+        <div style={{ display: "flex", justifyContent: "space-around" }}>
+          <Input.Search
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+            style={{ width: "25vw", marginTop: "20px" }}
+            placeholder="Search..."
+          />
+          <div
+            style={{ display: "flex", flexDirection: "column", width: "30%" }}
+          >
+            <Filters
+              type={type}
+              setType={setType}
+              price={price}
+              setPrice={setPrice}
+            />
+          </div>
+        </div>
       </div>
       <div className="prod-list">
         {list.length > 0 ? (
